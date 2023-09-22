43da8b9898f646bdc7185601ad1911bc78e9d6d7
Yulia Harbenkova <harben@MacBook-Pro-Viktor-2.local>
2023-09-22 20:26:08 +0300

    store_book

---
+++ src/random_number/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from random import randint
+# Create your views here.
+
+def get_random(request):
+    return HttpResponse('Hello, world!!!')
